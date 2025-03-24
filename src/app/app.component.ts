5c6ba728a3f0b974c2f582dbd0135d056891e0b5
Vinicius Yukio Ishikawa <viniyishikawa@gmail.com>
2025-03-24 20:48:36 -0300

    WIP: Criado protótipo do carrossel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatButtonModule,
+    MatCardModule,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend-hortifruti';
+
+  images: string[] = [
+    '../assets/images/download1.jpeg',
+    '../assets/images/download2.jpeg',
+    '../assets/images/download.jpeg'
+  ];
+  currentIndex = 0;
+
+  next() {
+    if (this.currentIndex < this.images.length - 1) {
+      this.currentIndex++;
+    } else {
+      this.currentIndex = 0; // Loop infinito
+    }
+  }
+
+  prev() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    } else {
+      this.currentIndex = this.images.length - 1; // Voltar para última imagem
+    }
+  }
 }
